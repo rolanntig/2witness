7e711d2f0d2a031a6cbc93de4e43a8c3760cf332
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2024-03-06 15:21:18 +0100

    navbar och kamera

--- App.js
+++ App.js
@@ -1,36 +1,86 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { Settings, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+import CameraScreen from "./src/screens/homeScreen";
+import MapScreen from "./src/screens/mapScreen";
+import SettingsScreen from "./src/screens/settingsScreen";
+import InfoScreen from "./src/screens/infoScreen";
+import ProfileScreen from "./src/screens/profileScreen";
 
-import HomeScreen from './src/screens/homeScreen';
-import SettingsScreen from './src/screens/settingsScreen';
-import InfoScreen from './src/screens/infoScreen';
-import MapScreen from './src/screens/mapScreen';
-import UploadScreen from './src/screens/uploadScreen';
-import ProfileScreen from './src/screens/profileScreen';
 
-const Stack = createNativeStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} options={{
-          headerStyle: {
-            backgroundColor: '#404654',
-            borderBottomColor: '#404654',
-            color: '#ffffff',
-          },
-        }} />
-        <Stack.Screen name="Info" component={InfoScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Upload" component={UploadScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+	return (
+		<Tab.Navigator
+			initialRouteName="Feed"
+			screenOptions={{
+				tabBarActiveTintColor: "#e91e63",
+			}}
+		>
+			<Tab.Screen
+				name="Camera"
+				component={CameraScreen}
+				options={{
+					tabBarLabel: "Home",
+					tabBarIcon: ({ color, size }) => (
+						<AntDesign name="camera" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Info"
+				component={InfoScreen}
+				options={{
+					tabBarLabel: "Info",
+					tabBarIcon: ({ color, size }) => (
+						<AntDesign name="infocirlceo" color={color} size={size} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Profile"
+				component={ProfileScreen}
+				options={{
+					tabBarLabel: "profile",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name="account" color={color} size={size} />
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name="map"
+				component={MapScreen}
+				options={{
+					tabBarLabel: "map",
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name="map" color={color} size={size} />
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name="settings"
+				component={SettingsScreen}
+				options={{
+					tabBarLabel: "setting",
+					tabBarIcon: ({ color, size }) => (
+						<AntDesign name="setting" color={color} size={size} />
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
 }
 
-export default App;
+export default function App() {
+	return (
+		<NavigationContainer>
+			<MyTabs />
+		</NavigationContainer>
+	);
+}
