e6fb40dd65797a0e6174858d7f5b8b043be9d1b3
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2024-03-07 15:35:10 +0100

    många saker

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,124 +1,229 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, TouchableOpacity, Text, Alert } from 'react-native';
-import { Camera } from 'expo-camera';
-import * as FileSystem from 'expo-file-system';
-import * as MediaLibrary from 'expo-media-library';
+import React, { useState, useEffect, useRef } from "react";
+import { View, TouchableOpacity, Text, Alert } from "react-native";
+import { Camera } from "expo-camera";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
+import {
+	MaterialCommunityIcons,
+	AntDesign,
+	MaterialIcons,
+} from "@expo/vector-icons";
+import { Audio } from "expo-av";
 
 function CameraScreen() {
-  const [hasCameraPermission, setHasCameraPermission] = useState(null);
-  const [hasAudioPermission, setHasAudioPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const cameraRef = useRef(null);
+	const [hasCameraPermission, setHasCameraPermission] = useState(null);
+	const [hasAudioPermission, setHasAudioPermission] = useState(null);
+	const [type, setType] = useState(Camera.Constants.Type.back);
+	const cameraRef = useRef(null);
+	const [isRecording, setIsRecording] = useState(false);
+	const recordingRef = useRef(new Audio.Recording());
 
-  useEffect(() => {
-    (async () => {
-      const cameraStatus = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(cameraStatus.status === 'granted');
+	useEffect(() => {
+		(async () => {
+			const cameraStatus = await Camera.requestCameraPermissionsAsync();
+			setHasCameraPermission(cameraStatus.status === "granted");
 
-      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
-      setHasAudioPermission(audioStatus.status === 'granted');
-    })();
-  }, []);
+			const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+			setHasAudioPermission(audioStatus.status === "granted");
+		})();
+	}, []);
 
-  if (hasCameraPermission === null || hasAudioPermission === null) {
-    return <View />;
-  }
-  if (hasCameraPermission === false || hasAudioPermission === false) {
-    return <Text>Ingen åtkomst till kameran eller mikrofonen</Text>;
-  }
+	if (hasCameraPermission === null || hasAudioPermission === null) {
+		return <View />;
+	}
+	if (hasCameraPermission === false || hasAudioPermission === false) {
+		return <Text>Ingen åtkomst till kameran eller mikrofonen</Text>;
+	}
 
-  const saveMedia = async (media) => {
-    try {
-      // Spara media till appens dokumentmapp
-      const fileName = media.uri.split('/').pop();
-      const newPath = FileSystem.documentDirectory + fileName;
-      await FileSystem.moveAsync({
-        from: media.uri,
-        to: newPath,
-      });
+	const saveMedia = async (media) => {
+		try {
+			const fileName = media.uri.split("/").pop();
+			const newPath = FileSystem.documentDirectory + fileName;
+			await FileSystem.moveAsync({
+				from: media.uri,
+				to: newPath,
+			});
 
-      // Begär tillstånd och spara media till enhetens galleri
-      const { status } = await MediaLibrary.requestPermissionsAsync();
-      if (status === 'granted') {
-        const asset = await MediaLibrary.createAssetAsync(newPath);
-        await MediaLibrary.createAlbumAsync('Video', asset, false);
-      } else {
-        Alert.alert('Tillstånd krävs', 'Tillstånd att spara filer krävs.');
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
+			const { status } = await MediaLibrary.requestPermissionsAsync();
+			if (status === "granted") {
+				const asset = await MediaLibrary.createAssetAsync(newPath);
+				await MediaLibrary.createAlbumAsync("Video", asset, false);
+			} else {
+				Alert.alert("Tillstånd krävs", "Tillstånd att spara filer krävs.");
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	};
 
-  const takePicture = async () => {
-    if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();
-      await saveMedia(photo);
-    }
-  };
+	const takePicture = async () => {
+		if (cameraRef.current) {
+			const photo = await cameraRef.current.takePictureAsync();
+			await saveMedia(photo);
+		}
+	};
 
-  const startRecording = async () => {
-    if (cameraRef.current) {
-      try {
-        const video = await cameraRef.current.recordAsync();
-        await saveMedia(video);
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  };
+	const startRecording = async () => {
+		if (cameraRef.current) {
+			try {
+				const video = await cameraRef.current.recordAsync();
+				await saveMedia(video);
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	};
 
-  const stopRecording = () => {
-    if (cameraRef.current) {
-      cameraRef.current.stopRecording();
-    }
-  };
+	const stopRecording = () => {
+		if (cameraRef.current) {
+			cameraRef.current.stopRecording();
+		}
+	};
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back,
-              );
-            }}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-              Vänd
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onLongPress={startRecording}
-            onPressOut={stopRecording}
-            onPress={takePicture}
-            style={{
-              alignSelf: 'center',
-              alignItems: 'center',
-              justifyContent: 'center',
-              marginBottom: 20,
-            }}
-          >
-            <Text style={{ fontSize: 18, color: 'white' }}> Ta Bild/Film </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
+	const recordAudio = async () => {
+		try {
+			await Audio.requestPermissionsAsync();
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: true,
+				playsInSilentModeIOS: true,
+			});
+
+			// Skapa en ny inspelning
+			const recording = new Audio.Recording();
+			await recording.prepareToRecordAsync(
+				Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
+			);
+			await recording.startAsync();
+			recordingRef.current = recording;
+			setIsRecording(true);
+			console.log("Inspelning pågår...");
+		} catch (error) {
+			console.error("Ett fel uppstod under start av ljudinspelning:", error);
+		}
+	};
+
+	const stopRecordingAudio = async () => {
+		try {
+			const recording = recordingRef.current;
+			await recording.stopAndUnloadAsync();
+			const uri = recording.getURI();
+			console.log("Inspelning avslutad och sparad på:", uri);
+			setIsRecording(false);
+
+			if (uri) {
+				const { status } = await MediaLibrary.requestPermissionsAsync();
+				if (status === "granted") {
+					const asset = await MediaLibrary.createAssetAsync(uri);
+					const album = await MediaLibrary.getAlbumAsync("Mina Filer");
+					if (album) {
+						await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
+					} else {
+						await MediaLibrary.createAlbumAsync("Mina Filer", asset, false);
+					}
+					console.log("Inspelning sparad i filsystemet!");
+				} else {
+					console.log("Åtkomst till mediabiblioteket nekad.");
+				}
+			} else {
+				console.log("Ingen inspelningsfil att spara.");
+			}
+		} catch (error) {
+			console.error("Ett fel uppstod under stopp av ljudinspelning:", error);
+		}
+	};
+
+	const uploadMedia = async () => {
+		// Låt användaren välja en bild
+		const result = await ImagePicker.launchImageLibraryAsync({
+		  mediaTypes: ImagePicker.MediaTypeOptions.All,
+		  allowsEditing: true,
+		  aspect: [4, 3],
+		  quality: 1,
+		});
+	  
+		if (!result.cancelled) {
+		  // Använd FileSystem.uploadAsync för att ladda upp bilden
+		  const localUri = result.uri;
+		  const filename = localUri.split('/').pop();
+		  const match = /\.(\w+)$/.exec(filename);
+		  const type = match ? `image/${match[1]}` : "image";
+	  
+		  // Ange URL till din uppladdningsserver och inkludera mappen 'media'
+		  const url = '../homeScreen/media';
+	  
+		  const formData = new FormData();
+		  formData.append('photo', { uri: localUri, name: filename, type });
+	  
+		  const options = {
+			method: 'POST',
+			body: formData,
+			headers: {
+			  Accept: 'application/json',
+			  'Content-Type': 'multipart/form-data',
+			},
+		  };
+	  
+		  return await FileSystem.uploadAsync(url, localUri, options);
+		}
+	  };
+	  
+
+	return (
+		<View style={{ flex: 1 }}>
+			<Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: "transparent",
+						flexDirection: "row",
+						justifyContent: "space-around",
+						alignItems: "flex-end",
+						paddingBottom: 20,
+					}}
+				>
+					<TouchableOpacity
+						onPress={() => {
+							setType(
+								type === Camera.Constants.Type.back
+									? Camera.Constants.Type.front
+									: Camera.Constants.Type.back,
+							);
+						}}
+					>
+						<MaterialCommunityIcons
+							name="camera-switch"
+							size={32}
+							color="white"
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={uploadMedia}>
+						<AntDesign name="clouduploado" size={32} color="white" />
+					</TouchableOpacity>
+					<TouchableOpacity
+						onLongPress={startRecording}
+						onPressOut={stopRecording}
+						onPress={takePicture}
+					>
+						<MaterialCommunityIcons
+							name="record-circle-outline"
+							size={100}
+							color="white"
+						/>
+					</TouchableOpacity>
+					<TouchableOpacity
+						onPress={isRecording ? stopRecordingAudio : recordAudio}
+					>
+						<MaterialIcons
+							name={isRecording ? "mic-off" : "mic"}
+							size={32}
+							color="white"
+						/>
+					</TouchableOpacity>
+				</View>
+			</Camera>
+		</View>
+	);
 }
 
 export default CameraScreen;
--- src/screens/mapScreen/index.js
+++ src/screens/mapScreen/index.js
@@ -1,27 +1,61 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { Entypo } from '@expo/vector-icons';
 
-export default function MapScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <Text>Map</Text>
-            <Button
-                title="Go to Home"
-                onPress={() =>
-                    navigation.navigate('Home')
-                }
-            />
-            <StatusBar style="auto" />
-        </View>
-    );
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {location ? (
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Min position"
+            description="Här är jag just nu.">
+            <Entypo name="location-pin" size={24} color="black" />
+          </Marker>
+        </MapView>
+      ) : null}
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
 });
