dcf0c01e0e5b57ec4ce8eaa6c28fe75517b1e37b
Adam Hammadi <adam.hammadi@elev.ga.ntig.se>
2024-03-08 13:42:55 +0100

    infoScreen

--- src/screens/infoScreen/index.js
+++ src/screens/infoScreen/index.js
@@ -1,27 +1,45 @@
-import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-export default function InfoScreen({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <Text>Info</Text>
-            <Button
-                title="Go to Home"
-                onPress={() =>
-                    navigation.navigate('Home')
-                }
-            />
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+const InfoScreen = () => {
+	const navigation = useNavigation();
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+	return (
+		<View className="flex-1 bg-white items-center">
+			<Text className="text-3xl font-bold mb-6">Information</Text>
+
+			<View className="flex flex-row space-x-2 justify-between">
+				<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6">
+					<View className="w-full ">
+						<Text className="text-gray-700">Vid akuta händelser ring:</Text>
+					</View>
+					<View className="w-full px-4">
+						<Text className="text-3xl font-bold text-center">112</Text>
+					</View>
+				</View>
+
+				<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6">
+					<View className="w-full ">
+						<Text className="text-gray-700">Vid andra ärenden ring:</Text>
+					</View>
+					<View className="w-full px-4">
+						<Text className="text-3xl font-bold text-center">114 14</Text>
+					</View>
+				</View>
+			</View>
+
+			<Text className="text-2xl font-bold mb-6">Eftersökt Information</Text>
+
+			<Button
+				title="Go to Home"
+				onPress={() => navigation.navigate("Home")}
+				className="bg-blue-500 py-2 px-4 rounded-lg text-white font-bold shadow-md"
+			/>
+			<StatusBar style="auto" />
+		</View>
+	);
+};
+
+export default InfoScreen;
