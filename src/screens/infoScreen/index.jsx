48e56b23906d8f014566d2e034b19dca8b8176ff
Danhub03 <118169550+Danhub03@users.noreply.github.com>
2024-03-12 14:00:13 +0100

    infopage

--- src/screens/infoScreen/index.jsx
+++ src/screens/infoScreen/index.jsx
@@ -7,20 +7,22 @@ import * as ImagePicker from "expo-image-picker";
 const InfoScreen = () => {
 	const [wantedInfo, setWantedInfo] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-        const data = await response.json();
-        setWantedInfo(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Implementera lämplig felhantering här
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					"https://jsonplaceholder.typicode.com/todos",
+				);
+				const data = await response.json();
+				setWantedInfo(data);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+				// Implementera lämplig felhantering här
+			}
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
 	const uploadMedia = async () => {
 		try {
@@ -52,37 +54,37 @@ const InfoScreen = () => {
 	return (
 		<View className="flex-1 bg-white items-center p-4">
 			<ScrollView>
-			<Text className="text-3xl font-bold mb-6">Information</Text>
+				<Text className="text-3xl font-bold mb-6">Information</Text>
 
-			<View className="flex flex-row space-x-2 justify-between w-full">
-				<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6 flex-1">
-					<Text className="text-gray-700">Vid akuta händelser ring:</Text>
-					<Text className="text-3xl font-bold text-center">112</Text>
-				</View>
-
-				<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6 flex-1">
-					<Text className="text-gray-700">Vid andra ärenden ring:</Text>
-					<Text className="text-3xl font-bold text-center">114 14</Text>
-				</View>
-			</View>
-
-			<Text className="text-2xl font-bold mb-6">Eftersökt Information</Text>
-
-			<View className="bg-white max-h-screen h-3/4  border rounded-lg shadow-md w-full mb-6 whitespace-normal">
-				{wantedInfo.map((item) => (
-					<View
-						key={item.id}
-						className="flex border-b flex-row p-2 justify-between "
-					>
-						<Text className=" flex-1 tracking-wide">{item.title}</Text>
-						<TouchableOpacity onPress={() => uploadMedia()}>
-							<AntDesign name="clouduploado" size={32} color="black" />
-						</TouchableOpacity>
+				<View className="flex flex-row space-x-2 justify-between w-full">
+					<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6 flex-1">
+						<Text className="text-gray-700">Vid akuta händelser ring:</Text>
+						<Text className="text-3xl font-bold text-center">112</Text>
 					</View>
-				))}
-			</View>
 
-			<StatusBar style="auto" />
+					<View className="flex bg-white border rounded-lg shadow-md p-4 mb-6 flex-1">
+						<Text className="text-gray-700">Vid andra ärenden ring:</Text>
+						<Text className="text-3xl font-bold text-center">114 14</Text>
+					</View>
+				</View>
+
+				<Text className="text-2xl font-bold mb-6">Eftersökt Information</Text>
+
+				<View className="bg-white border overflow-y-scroll h-max-screen h-1/2 rounded-lg shadow-md w-full mb-6 whitespace-normal">
+					{wantedInfo.map((item) => (
+						<View
+							key={item.id}
+							className="flex border-b flex-row p-2 justify-between "
+						>
+							<Text className=" flex-1 tracking-wide">{item.title}</Text>
+							<TouchableOpacity onPress={() => uploadMedia()}>
+								<AntDesign name="clouduploado" size={32} color="black" />
+							</TouchableOpacity>
+						</View>
+					))}
+				</View>
+
+				<StatusBar style="auto" />
 			</ScrollView>
 		</View>
 	);
